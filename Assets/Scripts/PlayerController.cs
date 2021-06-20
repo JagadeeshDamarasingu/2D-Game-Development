37bc7704ca4365ff853ed36e246959c0c87340df
Jagadeesh Damarasingu <jagadeesh.damarasingu@gmail.com>
2021-06-20 13:21:21 +0530

    added code to take horizontal input and applied run and flip animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private static readonly int Speed = Animator.StringToHash("speed");
+
+    private void Update()
+    {
+        var speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat(Speed, Mathf.Abs(speed));
+
+        var scale = transform.localScale;
+        scale.x = Mathf.Abs(scale.x) * speed < 0 ? -1f : 1f;
+        transform.localScale = scale;
+    }
+}
