83f6f809e3662bd80c98d1d09035dc4ac94917b5
Jagadeesh Damarasingu <jagadeesh.damarasingu@gmail.com>
2021-06-22 09:42:25 +0530

    added tilemap and empty game object to detect level complete

---
+++ Assets/Scripts/LevelCompleteController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompleteController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Level Completed!!!");
+        }
+    }
+}
